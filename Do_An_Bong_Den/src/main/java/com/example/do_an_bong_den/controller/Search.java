38105caa354faef512c002b2c8e97f8f4f87cbad
Linh2404XD <147931651+Linh2404XD@users.noreply.github.com>
2024-01-23 21:05:19 +0700

    chỉnh sửa phần search

--- Do_An_Bong_Den/src/main/java/com/example/do_an_bong_den/controller/Search.java
+++ Do_An_Bong_Den/src/main/java/com/example/do_an_bong_den/controller/Search.java
@@ -7,23 +7,30 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 
 @WebServlet(name = "Search", value = "/search")
 public class Search extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String search = request.getParameter("search");
-        Dao dao = new Dao();
-//        List<Product> list = dao.searchbyname(search);
+        // search bằng tiếng việt
         request.setCharacterEncoding("UTF-8");
+        // lấy giá trị từ khóa tìm kiếm
+        String search = request.getParameter("keyword");
+//        Dao dao = new Dao();
+//        List<Product> list = dao.searchbyname(search);
+//        request.setCharacterEncoding("UTF-8");
 //        request.setAttribute("product", list);
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+//        request.getRequestDispatcher("searchsp.jsp").forward(request, response);
 
+//        response.sendRedirect("searchsp.jsp?keyword=" + URLEncoder.encode(search, "UTF-8"));
+        request.getRequestDispatcher("searchsp.jsp").forward(request, response);
+// chuyển hướng người dùng đến trang kết quả tìm kiếm
     }
 }
